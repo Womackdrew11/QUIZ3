4016c6ae6f66adcdac0a9693d51b0a2de8e5c5d7
Womackdrew11 <157870565+Womackdrew11@users.noreply.github.com>
2024-01-27 21:57:58 -0600

    Add files via upload

---
+++ quiz3.py
@@ -0,0 +1,11 @@
+
+
+def main():
+
+    name = input("Enter your name: ")
+    age = input("Enter your age: ")
+
+    print(f"Hello, {name}! You are {age} years old.")
+
+if __name__ == "__main__":
+    main()
