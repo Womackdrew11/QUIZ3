3c4fbb8d7fd6c8d4a8eca8533fe8033031d9ce45
Womackdrew11 <157870565+Womackdrew11@users.noreply.github.com>
2024-01-27 23:07:10 -0600

    Update quiz3.py

--- quiz3.py
+++ quiz3.py
@@ -5,7 +5,7 @@ def main():
     name = input("Enter your name: ")
     age = input("Enter your age: ")
 
-    print(f"Hello, {name}! You are {age} years old.")
+    print(f"Hello, {name}! You are {age} yearsold.")
 
 if __name__ == "__main__":
     main()
